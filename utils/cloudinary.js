b1e433476d0f078a4a8a498893c1c16529b687ba
BenjiOdhis <nehemiahbenjamin7@gmail.com>
2025-04-22 14:35:36 +0300

    another log

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,11 +1,14 @@
 const cloudinary = require('cloudinary').v2;
 
 require('dotenv').config(); // at the top
+console.log("🔐 CLOUD_NAME:", process.env.CLOUD_NAME);
+console.log("🔐 CLOUD_API_KEY:", process.env.CLOUD_API_KEY);
+console.log("🔐 CLOUD_API_SECRET:", process.env.CLOUD_API_SECRET);
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
 });
 
 
